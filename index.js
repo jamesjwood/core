9b5dd1adca8f5fe2581921a599233d13cafae392
jamesjwood <jamesjwood@msn.com>
2013-10-30 13:52:58 +0000

    update dependencies

--- index.js
+++ index.js
@@ -232,34 +232,15 @@ exports.updatePrivateValue = function (privateObject, publicObject, propertyName
   }
 };
 
-exports.eraseCookie = function (name) {
-    exports.createCookie(name, "", -1);
+  exports.eraseProperty = function (name) {
+    window.localStorage.removeItem(name);
   };
-  exports.createCookie = function (name, value, days) {
-    var expires = null;
-    if (days) {
-      var date = new Date();
-      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-      expires = "; expires=" + date.toGMTString();
-    } else {
-      expires = "";
-    }
-    window.document.cookie = name + "=" + value + expires + "; path=/";
+  exports.persistProperty = function (name, value) {
+    window.localStorage.setItem(name, value);
   };
   
-  exports.readCookie = function (name) {
-    var nameEQ = name + "=";
-    var ca = window.document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) === ' ') {
-        c = c.substring(1, c.length);
-      }
-      if (c.indexOf(nameEQ) === 0) {
-        return c.substring(nameEQ.length, c.length);
-      }
-    }
-    return null;
+  exports.readProperty = function (name) {
+    return window.localStorage.getItem(name);
   };
 
 exports.addProperty = function (object, propertyName, initialValue, privateObject, access) {
